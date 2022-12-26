7ca0c4d4b377653b9125556dbfa98eafc970a10a
Daniyar <baddaniar@gmail.com>
2022-12-26 15:24:58 +0600

    hw5 done

--- HW5/server/app.js
+++ HW5/server/app.js
@@ -9,49 +9,8 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: "false" }));
 
 
-// //Получение файла из папки public
-// app.use("/static", express.static(__dirname +`/public`))
-
-// let usersArray = [
-//     { id: 1, department: "IT", name: "Daniar" },
-//     { id: 2, department: "Audit", name: "Aslan" },
-// ];
-
-// //GET запросы
-// app.get("/users", (req, res) => {
-//     res.send(usersArray);
-// });
-
-// app.get("/users/:id", (req, res) => {
-//     const { id } = req.params;
-//     res.send(usersArray.find((item) => item.id === +id));
-// });
-
-// app.get("/users/department/:name", (req, res) => {
-//     const { name } = req.params;
-//     res.send(usersArray.filter((item) => item.department === name));
-// });
-
-// //POST запросы
-// app.post("/users", (req, res) => {
-//     const {id, department, name} = req.body
-//     usersArray.push({id, department, name})
-//     res.status(201).send("ok")
-// });
-
-// app.put("/users", (req, res) => {
-//     const {id, department, name} = req.body
-//     userIndex = usersArray.findIndex(item => item.id === +id)
-//     usersArray[userIndex].name = name
-//     usersArray[userIndex].department = department
-//     res.status(201).send("ok")
-// });
-
-
-//Задачка
 
 app.get("/cars", (req, res) =>{
-    console.log("Cars router")
     res.send(fs.readFileSync("./cars.json", {encoding: "utf-8"}));
 });
 
@@ -62,7 +21,7 @@ app.post("/cars", (req, res) =>{
 });
 
 app.put("/cars", (req, res) =>{
-    const {id, model} = res.body
+    const {id, model} = req.body
     let carsArray = JSON.parse(fs.readFileSync("./cars.json", {encoding: "utf-8"}))
     carsIndex = carsArray.findIndex(item => item.id === +id)
     carsArray[carsIndex].model = model
@@ -71,7 +30,7 @@ app.put("/cars", (req, res) =>{
 });
 
 app.delete("/cars/:id", (req, res) =>{
-    const {id} = req.body
+    const id = +req.params.id
     const carsArray = JSON.parse(fs.readFileSync("./cars.json", {encoding: "utf-8"}))
     const newArrray = carsArray.filter(item => item.id !== +id)
     fs.writeFileSync("./cars.json", JSON.stringify(newArrray));
